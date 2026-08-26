24dd0f24409ad43d6c8ec9783004b48026027395
Rrcanrcode31 <trueman0711@gmail.com>
2026-08-26 09:47:49 +0800

    fixed session token

--- web-based/project.js
+++ web-based/project.js
@@ -42,7 +42,7 @@ app.use(session({
     saveUninitialized: false,
     store: sessionStore,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days, adjust as needed
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }));
 
